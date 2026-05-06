925df4995559241d12bd96f5f9b9d659ce1a888e
Thomas Loubrieu <loubrieu@jpl.nasa.gov>
2026-05-05 19:49:22 -0700

    performance improvement needed for big Europa-Clipper MISE product, the code was not well written in the first place.

--- src/spac_kit/parser/parse_ccsds_downlink.py
+++ src/spac_kit/parser/parse_ccsds_downlink.py
@@ -262,7 +262,7 @@ def distribute_packets(keyss, stream1):
     rows = ccsdspy.utils.split_packet_bytes(stream1)
     for i, key in enumerate(keyss):
         if key not in buffers:
-            buffers[key] = bytes()
+            buffers[key] = []
         buffers[key].append(rows[i])
     buffers = {k: io.BytesIO(b"".join(v)) for k, v in buffers.items()}
     return buffers
